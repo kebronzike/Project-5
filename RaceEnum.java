15d7415cee044e8a33e7232734ebe945f524cdd0
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-18 18:42:32 -0500

    Update RaceEnum.java

--- RaceEnum.java
+++ RaceEnum.java
@@ -1,10 +1,27 @@
 package prj5;
 
+//Virginia Tech Honor Code Pledge:
+//
+//As a Hokie, I will conduct myself with honor and integrity at all times.
+//I will not lie, cheat, or steal, nor will I accept the actions of those who
+//do.
+//-- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
+//(kebronZike)
+
+//-------------------------------------------------------------------------
+/**
+* Sets standard race names
+* 
+* @author Joshua Murphy (jmmurphy)
+* @author Connor Pepin (connorpepin)
+* @author Kebron Zike (kebronZike)
+* @version 2021.11.15
+* 
+*/
 public enum RaceEnum {
-    
+   
     /**
      * this will represent the different races displayed
      */
     ASIAN, BLACK, WHITE, LATINX, OTHER
-
 }
