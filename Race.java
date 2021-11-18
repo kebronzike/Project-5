6716af49c55748f4e03b567206985017fb976fad
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-18 18:40:28 -0500

    Update Race.java

--- Race.java
+++ Race.java
@@ -1,5 +1,24 @@
 package prj5;
 
+//Virginia Tech Honor Code Pledge:
+//
+//As a Hokie, I will conduct myself with honor and integrity at all times.
+//I will not lie, cheat, or steal, nor will I accept the actions of those who
+//do.
+//-- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
+//(kebronZike)
+
+//-------------------------------------------------------------------------
+/**
+* This class creates race objects that store the 
+* the covid cases and deaths
+* 
+* @author Joshua Murphy (jmmurphy)
+* @author Connor Pepin (connorpepin)
+* @author Kebron Zike (kebronZike)
+* @version 2021.11.15
+* 
+*/
 public class Race {
     private RaceEnum enumrace;
     private String fatalities;
