e6aabaff125033794b0b9a5209abeed405a70792
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-18 18:46:14 -0500

    Update RaceTest.java

--- RaceTest.java
+++ RaceTest.java
@@ -1,5 +1,23 @@
 package prj5;
 
+//Virginia Tech Honor Code Pledge:
+//
+//As a Hokie, I will conduct myself with honor and integrity at all times.
+//I will not lie, cheat, or steal, nor will I accept the actions of those who
+//do.
+//-- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
+//(kebronZike)
+
+//-------------------------------------------------------------------------
+/**
+* Tests the Race Class
+* 
+* @author Joshua Murphy (jmmurphy)
+* @author Connor Pepin (connorpepin)
+* @author Kebron Zike (kebronZike)
+* @version 2021.11.15
+* 
+*/
 public class RaceTest extends student.TestCase {
     private Race tester;
     private RaceEnum enumrace;
