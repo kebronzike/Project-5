02767fdc26f5a3531d434754c4b45b720a0451d3
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-18 18:33:05 -0500

    Update Input.java

--- Input.java
+++ Input.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 //As a Hokie, I will conduct myself with honor and integrity at all times.
 //I will not lie, cheat, or steal, nor will I accept the actions of those who
 //do.
-//-- Joshua Murphy (Jmmurphy)
+//-- Joshua Murphy && Kebron Zike && Connor Pepin
 
 //-------------------------------------------------------------------------
 /**
