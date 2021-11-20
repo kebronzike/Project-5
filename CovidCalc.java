02a4ef99c8872fa59606930dd773808f7ce50501
kebronzike <74264545+kebronzike@users.noreply.github.com>
2021-11-20 13:02:33 -0500

    Add files via upload

--- CovidCalc.java
+++ CovidCalc.java
@@ -3,16 +3,15 @@
  */
 package prj5;
 
-
-//Virginia Tech Honor Code Pledge:
+// Virginia Tech Honor Code Pledge:
 //
-//As a Hokie, I will conduct myself with honor and integrity at all times.
-//I will not lie, cheat, or steal, nor will I accept the actions of those who
-//do.
-//-- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
-//(kebronZike)
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
+// (kebronZike)
 
-//-------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 
 /**
  * Implements the sorting functions so you can
@@ -28,170 +27,135 @@ package prj5;
  */
 public class CovidCalc {
 
-    
-   
     LinkedList<Race>[] unsortedStates;
     LinkedList<Race>[] sortedStates;
-    
-    
-    
+
     /**
      * Constructor for the CovidCalc class
      */
-    public CovidCalc(LinkedList<Race>[] states)
-    {
+    public CovidCalc(LinkedList<Race>[] states) {
         this.unsortedStates = states;
     }
-    
-    
-    
-    
+
+
     /**
      * Takes an unsorted list of races and sorts them
      * based on their alphabetically by state
      * 
-     * @param unsortedList the list you want sorted
+     * @param unsortedList
+     *            the list you want sorted
      * @return the list after it has been sorted
      */
     @SuppressWarnings("unchecked")
-    public LinkedList<Race>[] sortByAlpha(LinkedList<Race>[] unsortedArray)
-    {
-        //make a blank array the length of the unsorted one
+    public LinkedList<Race>[] sortByAlpha(LinkedList<Race>[] unsortedArray) {
+        // make a blank array the length of the unsorted one
         LinkedList<Race>[] newArray = new LinkedList[unsortedArray.length];
-        
+
         int length = unsortedArray.length;
-        
-        for (int i = 0; i < length; i++)
-        {
-            
-            //get initial value for alphabetical
+
+        for (int i = 0; i < length; i++) {
+
+            // get initial value for alphabetical
             String alpha = unsortedArray[0].get(0).getState();
-            
+
             int index = 0;
-            
-            //loop through list to find smaller alpha
-            for (int j = 1; j < unsortedArray.length; j++)
-            {
-                
+
+            // loop through list to find smaller alpha
+            for (int j = 1; j < unsortedArray.length; j++) {
+
                 String potential = unsortedArray[j].get(0).getState();
-                
-                //if potential is smaller than alpha lexicographically
-                if (alpha.compareTo(potential) > 0)
-                {
-                    //store the index of this List
+
+                // if potential is smaller than alpha lexicographically
+                if (alpha.compareTo(potential) > 0) {
+                    // store the index of this List
                     index = j;
-                    
-                    //set alpha to the new alphabetical
+
+                    // set alpha to the new alphabetical
                     alpha = potential;
                 }
- 
+
             }
-            
-            //Add to the new array
+
+            // Add to the new array
             newArray[i] = unsortedArray[index];
-            
-            //remove from the unsorted Array
+
+            // remove from the unsorted Array
             @SuppressWarnings("rawtypes")
             LinkedList<Race> filler = new LinkedList();
             Race temp = new Race(RaceEnum.ASIAN, "1", "1", "ZZZ");
             filler.add(temp);
             unsortedArray[index] = filler;
         }
-        
-        
-        
-        //Set our sorted states field to our now sorted array
+
+        // Set our sorted states field to our now sorted array
         this.sortedStates = newArray;
-        
+
         return newArray;
-        
-        
+
     }
-    
-    
-    
-    
+
+
     /**
      * Takes an unsorted list of races and sorts them
      * based on their CFR
      * 
-     * @param unsortedList the list you want sorted
+     * @param unsortedList
+     *            the list you want sorted
      * @return the list after it has been sorted
      */
-    public LinkedList<Race> sortByCFR(LinkedList<Race> unsortedList)
-    {
-        
-        
+    public LinkedList<Race> sortByCFR(LinkedList<Race> unsortedList) {
+
         LinkedList<Race> newList = new LinkedList<Race>();
-        
+
         int size = unsortedList.size();
-        
-        
-        
-        
-        
-        for (int i = 0; i < size; i++)
-        {
-            //get initial value for largest CFR
+
+        for (int i = 0; i < size; i++) {
+            // get initial value for largest CFR
             int largestCFR = calculateCFR(unsortedList.get(0));
-            
+
             int index = 0;
-            
-            
-            //loop through the unsorted list and find the largest CFR
-            for (int j = 1; j < unsortedList.size(); j++)
-            {
+
+            // loop through the unsorted list and find the largest CFR
+            for (int j = 1; j < unsortedList.size(); j++) {
                 int potential = calculateCFR(unsortedList.get(j));
-                
-                //if a larger CFR is found then set that to largest
-                if (largestCFR < potential)
-                {
-                    //store the index this largest is at
+
+                // if a larger CFR is found then set that to largest
+                if (largestCFR < potential) {
+                    // store the index this largest is at
                     index = j;
-                    
-                    //set the new largest
+
+                    // set the new largest
                     largestCFR = potential;
                 }
             }
-            
-            
-            //add the largest value to the new list
+
+            // add the largest value to the new list
             newList.add(unsortedList.get(index));
-            
-            //remove that Race object from the unsortedList
+
+            // remove that Race object from the unsortedList
             unsortedList.remove(index);
         }
-        
-        
-        
-        
+
         return newList;
-        
+
     }
-    
-    
-    
-    
-    
-    
+
+
     /**
      * Takes the deaths and cases of a race
      * and calulates the CFR
      * 
      * 
-     * @param deaths the number of deaths
-     * @param cases the number of cases
+     * @param deaths
+     *            the number of deaths
+     * @param cases
+     *            the number of cases
      * @return the Covid Fatality Ratio
      */
-    private int calculateCFR(Race race)
-    {
-        
+    private int calculateCFR(Race race) {
+
         return (race.getDeaths() / race.getCases()) * 100;
-        
+
     }
-    
-    
-    
-    
-    
+
 }
