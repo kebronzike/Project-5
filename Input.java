1b7b9ba223b9c540a80f9fc8da09cc5e7cf73a43
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-21 14:28:20 -0600

    Update Input.java

--- Input.java
+++ Input.java
@@ -2,14 +2,14 @@ package prj5;
 
 import java.io.FileNotFoundException;
 
-//Virginia Tech Honor Code Pledge:
+// Virginia Tech Honor Code Pledge:
 //
-//As a Hokie, I will conduct myself with honor and integrity at all times.
-//I will not lie, cheat, or steal, nor will I accept the actions of those who
-//do.
-//-- Joshua Murphy && Kebron Zike && Connor Pepin
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Joshua Murphy (Jmmurphy)
 
-//-------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 /**
  * This class runs the project
  * 
@@ -19,8 +19,14 @@ import java.io.FileNotFoundException;
  * @version 2021.11.15
  */
 public class Input {
+    @SuppressWarnings("unused")
     public static void main(String[] args) throws FileNotFoundException {
-        @SuppressWarnings("unused")
-        Reader project = new Reader("Cases_and_Deaths_by_race_CRDT_Sep2020.csv");
-    } 
+        if (args.length == 1) {
+            Reader state = new Reader(args[0]);
+        }
+        else {
+            Reader state = new Reader(
+                "Cases_and_Deaths_by_race_CRDT_Sep2020.csv");
+        }
+    }
 }
