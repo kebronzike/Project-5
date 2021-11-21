a06fd59cd05d1b47cf80df1e9a8b145480552124
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-21 14:43:25 -0600

    Update CompareAlpha.java

--- CompareAlpha.java
+++ CompareAlpha.java
@@ -35,11 +35,8 @@ public class CompareAlpha implements Comparator<Race> {
         if (o1.getRace().compareTo(o2.getRace()) < 0) {
             return -1;
         }
-        else if (o1.getRace().compareTo(o2.getRace()) > 0) {
-            return 1;
-        }
         else {
-            return 0;
+            return 1;
         }
     }
 }
