b87f26a9ec4e08049fa0ea06f81e65908e97e6d2
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-21 14:22:36 -0600

    Update CompareAlphaTest.java

--- CompareAlphaTest.java
+++ CompareAlphaTest.java
@@ -1,5 +1,23 @@
 package prj5;
 
+//Virginia Tech Honor Code Pledge:
+//
+//As a Hokie, I will conduct myself with honor and integrity at all times.
+//I will not lie, cheat, or steal, nor will I accept the actions of those who
+//do.
+//-- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
+//(kebronZike)
+
+//-------------------------------------------------------------------------
+/**
+* This class tests the compareAlpha class
+* 
+* @author Joshua Murphy (jmmurphy)
+* @author Connor Pepin (connorpepin)
+* @author Kebron Zike (kebronZike)
+* @version 2021.11.15
+* 
+*/
 public class CompareAlphaTest extends student.TestCase {
 
     private CompareAlpha test1;
