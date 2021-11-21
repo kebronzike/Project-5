1e4a1e79a891d4c534b59d77def1c665607c9d8e
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-11-21 14:21:49 -0600

    Update CompareAlpha.java

--- CompareAlpha.java
+++ CompareAlpha.java
@@ -2,8 +2,34 @@ package prj5;
 
 import java.util.Comparator;
 
-public class CompareAlpha implements Comparator<Race>{
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Joshua Murphy (Jmmurphy), Connor Pepin (connorpepin), Kebron Zike
+// (kebronZike)
 
+// -------------------------------------------------------------------------
+/**
+ * Stores the data for the project
+ * 
+ * @author Joshua Murphy (jmmurphy)
+ * @author Connor Pepin (connorpepin)
+ * @author Kebron Zike (kebronZike)
+ * @version 2021.11.15
+ * 
+ */
+public class CompareAlpha implements Comparator<Race> {
+
+    /**
+     * Thia compares race objects by alpha
+     * 
+     * @param o1
+     *            the first race getting compared
+     * @param o2
+     *            the second race getting compared
+     */
     @Override
     public int compare(Race o1, Race o2) {
         if (o1.getRace().compareTo(o2.getRace()) < 0) {
@@ -16,5 +42,4 @@ public class CompareAlpha implements Comparator<Race>{
             return 0;
         }
     }
-
 }
