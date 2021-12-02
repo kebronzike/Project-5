283c2cd1fe89812abbc222a58355c6e3eac41d55
jmmurphy12 <94412601+jmmurphy12@users.noreply.github.com>
2021-12-02 18:16:15 -0500

    Update CovidWindow.java

--- CovidWindow.java
+++ CovidWindow.java
@@ -7,84 +7,239 @@ import cs2.TextShape;
 import cs2.Window;
 import cs2.WindowSide;
 
+/**
+ * Creates and operates the window on which the data for each
+ * states ethnicity is displayed
+ * 
+ * @author Luke Hylton (lnhylton) Shane Ervin (shaneerv13)
+ *         Jacob Albright (jacobalbright77)
+ * 
+ * @version 04.28.2021
+ */
 public class CovidWindow {
-
-    private Button[] stateButton;
-    private Button quit;
-    private Button sortAlpha;
-    private Button sortCFR;
     private Window window;
-    private CovidCalc calc;
+    private CovidCalc ethnicityCalculator;
+    private Button sortByAlpha;
+    private Button sortByCFR;
+    private Button quit;
+    private Button lastButton;
+    private Button[] stateButtons;
 
-    public CovidWindow(CovidCalc data) {
-        // Initiallizes window and CovidCalc objects
-        window = new Window("Covid Display");
-        calc = data;
-        // Adding all the buttons to window
-        int buttonSize = calc.getState().size();
-        stateButton = new Button[buttonSize];
-        for (int i = 0; i < buttonSize; i++) {
-            stateButton[i] = new Button("Reprsents " + calc.getState().get(i)
-                .getStateName());
-            window.addButton(stateButton[i], WindowSide.SOUTH);
-            stateButton[i].onClick(this);
-        }
+    /**
+     * constructor for the window
+     * 
+     * The window is optimized for full screen
+     * 
+     * @param calc
+     *            is the ethnicity calculator data passed
+     *            through
+     */
+    public CovidWindow(CovidCalc calc) {
+        ethnicityCalculator = calc;
+        window = new Window();
+        lastButton = null;
 
-        sortAlpha = new Button("Sort By Alpha");
-        window.addButton(sortAlpha, WindowSide.NORTH);
-        sortAlpha.onClick(this);
+        sortByAlpha = new Button("Sort by Alpha");
+        sortByAlpha.onClick(this, "clickedSortAlpha");
+
+        sortByCFR = new Button("Sort by CFR");
+        sortByCFR.onClick(this, "clickedSortCFR");
 
         quit = new Button("Quit");
-        window.addButton(quit, WindowSide.NORTH);
-        quit.onClick(this);
+        quit.onClick(this, "clickedQuit");
 
-        sortCFR = new Button("Sort By CFR");
-        window.addButton(sortCFR, WindowSide.NORTH);
-        sortCFR.onClick(this);
+        window.addButton(sortByAlpha, WindowSide.NORTH);
+        window.addButton(quit, WindowSide.NORTH);
+        window.addButton(sortByCFR, WindowSide.NORTH);
+
+        stateButtons = new Button[ethnicityCalculator.getState().size()];
+        for (int i = 0; i < ethnicityCalculator.getState().size(); i++) {
+            stateButtons[i] = new Button("Represent " + ethnicityCalculator
+                .getState().get(i).getStateName());
+            stateButtons[i].onClick(this, "clickedState");
+            window.addButton(stateButtons[i], WindowSide.SOUTH);
+        }
     }
 
 
     /**
-     * this will update the window when new commands are made in the program
+     * Exits the window
      * 
      * @param button
-     *            this will be used to update the window
+     *            is the quit button
+     */
+    public void clickedQuit(Button button) {
+        System.exit(0);
+    }
+
+
+    /**
+     * sorts ethnicity data by alphabetical order
+     * 
+     * @param button
+     *            is the alpha button
+     */
+    public void clickedSortAlpha(Button button) {
+        State state = getStateForButton(lastButton);
+        state.sortAlpha();
+        update(lastButton);
+    }
+
+
+    /**
+     * sorts data by CFR
+     * 
+     * @param button
+     *            is the CFR button
+     */
+    public void clickedSortCFR(Button button) {
+        State state = getStateForButton(lastButton);
+        state.sortCFR();
+        update(lastButton);
+    }
+
+
+    /**
+     * switches data to the clicked states data
+     * 
+     * @param button
+     *            is the state clicked
+     */
+    public void clickedState(Button button) {
+        lastButton = button;
+        update(button);
+    }
+
+
+    /**
+     * refreshes the window with the new images
+     * 
+     * @param button
+     *            tells the window to refresh
      */
     private void update(Button button) {
         window.removeAllShapes();
-        stateGUI(button);
-        textDisplay(button);
+        displayState(button);
+        displayText(button);
     }
 
 
     /**
-     * this will Display the text information
+     * displays the data for the state
      * 
      * @param button
-     *            this represents the state button
+     *            is the states button
      */
-    private void textDisplay(Button button) {
-        State state = stateButton(button);
-        String statenames = state.getStateName() + "CFR's by Race";
-        TextShape head = new TextShape((window.getWidth() / 2), (window
-            .getHeight() / 20), (statenames), Color.blue, 14);
-        head.setX(head.getX() - (head.getWidth() / 2));
-        window.addShape(head);
+    private void displayState(Button button) {
+        State state = getStateForButton(button);
+        double largeCFR = getLargestCFR(state);
+        int numState = state.getRaces().size();
+
+        for (int i = 0; i < state.getRaces().size(); i++) {
+            double sizeRatio = state.getRaces().get(i).calculateCFR()
+                / largeCFR;
+            int barHeight = (int)(((window.getHeight() / 2) * (sizeRatio)));
+            int barX = (window.getWidth() / state.getRaces().size()
+                * (i + 1)) - (window.getWidth() / 10);
+            int barY = (int)(((window.getHeight() / 2) - (window.getHeight()
+                / 3)) + ((window.getHeight() / 2) * (1 - sizeRatio)));
+            int barWidth = (window.getWidth() / (state.getRaces()
+                .size() * 10));
+            if (sizeRatio < 1) {
+                barY++;
+            }
+            if (sizeRatio > 0) {
+                window.addShape(new Shape(barX, barY, barWidth, barHeight,
+                    Color.BLUE));
+            }
+            else {
+                TextShape naText = new TextShape(barX, barY + barHeight, "NA",
+                    Color.BLACK, 80 / numState);
+                naText.setY(naText.getY() - naText.getHeight());
+                window.addShape(naText);
+            }
+            displayStateText(state.getRaces().get(i), barX, barY
+                + barHeight, numState);
+        }
+
     }
 
 
     /**
-     * this will get a state button depending on the state name
+     * displays the header
      * 
      * @param button
-     *            button for the state
-     * @return
-     *         the state
+     *            is the states button so it can
+     *            read the states name
      */
-    private State stateButton(Button button) {
-        for (int i = 0; i < stateButton.length; i++) {
-            if (button.equals(stateButton[i])) {
-                return calc.getState().get(i);
+    private void displayText(Button button) {
+        State state = getStateForButton(button);
+        String name = state.getStateName() + " Cases Fatality Ratios by Race";
+        TextShape header = new TextShape((window.getWidth() / 2), (window
+            .getHeight() / 20), (name), Color.BLACK, 14);
+        header.setX(header.getX() - (header.getWidth() / 2));
+        window.addShape(header);
+    }
+
+
+    /**
+     * displays the text for the state
+     * 
+     * @param ethnit
+     *            is the states ethnicity data
+     * @param xVal
+     *            location of the bar
+     * @param yVal
+     *            location of the bar
+     * @param numEth
+     *            effects the font size of the bar
+     */
+    private void displayStateText(
+        Race ethnit,
+        int xVal,
+        int yVal,
+        int numEth) {
+        int xLocal = xVal;
+        int yLocal = yVal;
+        StringBuilder build = new StringBuilder();
+        String ethnicityName = ethnit.getRace();
+        int fontSize = 80 / numEth;
+
+        TextShape barName = new TextShape(xLocal, yLocal, ethnicityName,
+            Color.BLACK, fontSize);
+        barName.setX(barName.getX() - (barName.getWidth() / 4));
+        barName.setY(barName.getY() + barName.getHeight());
+        window.addShape(barName);
+
+        if (ethnit.calculateCFR() != -1) {
+            if (((ethnit.calculateCFR() % 1) < 0.05)) {
+                int cfr = (int)ethnit.calculateCFR();
+                build.append(cfr + "%");
+            }
+            else {
+                build.append(String.format("%.1f", ethnit.calculateCFR()) + "%");
+            }
+
+            TextShape cfrString = new TextShape(barName.getX(), barName.getY(),
+                build.toString(), Color.BLACK, fontSize);
+            cfrString.setY(cfrString.getY() + (2 * cfrString.getHeight()));
+
+            window.addShape(cfrString);
+        }
+    }
+
+
+    /**
+     * returns the state for the correct button
+     * 
+     * @param button
+     *            the state trying to be accessed
+     * @return the state being accessed
+     */
+    private State getStateForButton(Button button) {
+        for (int i = 0; i < stateButtons.length; i++) {
+            if (button.equals(stateButtons[i])) {
+                return ethnicityCalculator.getState().get(i);
             }
         }
         return null;
@@ -92,97 +247,20 @@ public class CovidWindow {
 
 
     /**
-     * 
-     * @param button
-     */
-    private void stateGUI(Button button) {
-        State temp = stateButton(button);
-        double highestCFR = greatestCFR(temp);
-        int numberofstates = temp.getRaces().size();
-        for (int i = 0; i < temp.getRaces().size(); i++) {
-            double ratio = temp.getRaces().get(i).calculateCFR() / highestCFR;
-            int vertY = (int)(((window.getHeight() / 2) - (window.getHeight()
-                / 3)) + ((window.getHeight() / 2) * (1 - ratio)));
-            int vertX = (window.getWidth() / temp.getRaces().size() * (i + 1))
-                - (window.getWidth() / 10);
-            int vertHeight = (int)(((window.getHeight() / 2) * (ratio)));
-            int vertWidth = (window.getWidth() / (temp.getRaces().size() * 10));
-            if (ratio < 1) {
-                vertY++;
-            }
-            if (ratio > 0) {
-                window.addShape(new Shape(vertX, vertY, vertWidth, vertHeight,
-                    Color.BLUE));
-            }
-            else {
-                TextShape naText = new TextShape(vertX, vertY + vertHeight,
-                    "NA", Color.BLACK, 80 / numberofstates);
-                naText.setY(naText.getY() - naText.getHeight());
-                window.addShape(naText);
-            }
-            textState(temp.getRaces().get(i), vertX, vertY + vertHeight,
-                numberofstates);
-        }
-
-    }
-
-
-    private void textState(Race race, int vertX, int vertY, int size) {
-        StringBuilder str = new StringBuilder();
-        String racename = race.getRace();
-        int font = 100 / size;
-        int xvert = vertX;
-        int yvert = vertY;
-        TextShape vertBar = new TextShape(vertX, vertY, racename, Color.BLUE,
-            font);
-        vertBar.setY(vertBar.getY() + vertBar.getHeight());
-        vertBar.setX(vertBar.getX() - vertBar.getWidth() / 4);
-        window.addShape(vertBar);
-        if (race.calculateCFR() != -1) {
-            if ((race.calculateCFR() % 1) < 0.05) {
-                int cfrRatio = (int)race.calculateCFR();
-                str.append(cfrRatio + "%");
-            }
-            else {
-                str.append(String.format("%.1f", race.calculateCFR()) + "%");
-            }
-
-            TextShape cfrDisplay = new TextShape(vertBar.getX(), vertBar.getY(),
-                str.toString(), Color.BLACK, font);
-            cfrDisplay.setY(cfrDisplay.getY() + (2 * cfrDisplay.getHeight()));
-
-            window.addShape(cfrDisplay);
-        }
-    }
-
-
-    /**
-     * this will get the greatest CFR in a state
+     * gets the largest CFR
      * 
      * @param state
-     *            the state being checked
-     * @return
-     *         the highest cfr percent
+     *            the state being passed through
+     * @return the double for the highest CFR in that state
      */
-    private double greatestCFR(State state) {
-        double cFR = 0;
+    private double getLargestCFR(State state) {
+        double largestCFR = 0;
         for (int i = 0; i < state.getRaces().size(); i++) {
-            double temp = state.getRaces().get(i).calculateCFR();
-            if (cFR < temp) {
-                cFR = temp;
+            double tempCFR = state.getRaces().get(i).calculateCFR();
+            if (largestCFR < tempCFR) {
+                largestCFR = tempCFR;
             }
         }
-        return cFR;
+        return largestCFR;
     }
-
-
-    /**
-     * This method closes the window
-     * 
-     * @param button
-     */
-    public void clickedQuit(Button button) {
-        System.exit(0);
-    }
-
 }
